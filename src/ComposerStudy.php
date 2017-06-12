90f7af049c57e58c7c1afd552490018e2c989424
Paul <spaul@ukr.net>
2017-06-12 16:54:01 +0300

    Add files via upload

---
+++ src/ComposerStudy.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * composer-study test file
+ */
+namespace CS;
+
+class ComposerStudy{
+	function __construct(){
+		return 'You have successfully create new object of '.__CLASS__.' class!';
+	}
+}
+?>
